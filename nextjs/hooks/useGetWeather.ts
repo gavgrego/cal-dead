77c13197e4bfc51d73b252e8d7d5444ab92dca95
Gavin <gavin.a.gregory@gmail.com>
2022-04-06 17:21:04 -0700

    remove weather api token and put in env

--- nextjs/hooks/useGetWeather.ts
+++ nextjs/hooks/useGetWeather.ts
@@ -10,7 +10,7 @@ export type Weather = {
 const useGetWeather = () => {
   const { isLoading, error, data } = useQuery("getWeather", () =>
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?lat=37.9735346&lon=-122.5310874&appid=34eea8a1df134c110672f8a2b6fce63a&units=imperial"
+      `https://api.openweathermap.org/data/2.5/weather?lat=37.9735346&lon=-122.5310874&appid=${process.env.WEATHER_API_TOKEN}&units=imperial`
     ).then((res) => res.json())
   );
   return { isLoading, error, data };
